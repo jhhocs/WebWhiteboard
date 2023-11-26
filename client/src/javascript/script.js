a7cbaf844367cb045bfd1cbb40ad101fbb778fb7
jhhocs <joshuaho1@cpp.edu>
2023-11-25 16:29:03 -0800

    Fixed issue where lines are drawn twice

--- client/src/javascript/script.js
+++ client/src/javascript/script.js
@@ -1,20 +1,47 @@
+// https://stackoverflow.com/questions/44270239/how-to-get-socket-id-of-a-connection-on-client-side
+
 import io from "socket.io-client"
 setTimeout(() => {
    let canvas = document.getElementById('whiteboard');
    let context = canvas.getContext('2d');
    let toolbar = document.getElementById('toolbar');
+   let sessionID;
+   let current;
 
    const socket = io.connect("https://webwhiteboard.onrender.com");
+   //const socket = io.connect("http://localhost:3001"); // For local testing
+
+   socket.on('connect', () => {
+      sessionID = socket.id;
+
+      current = {
+         drawing: false,
+         color: 'black',
+         lineWidth: '2',
+         x: 0,
+         y: 0,
+         sessionID: sessionID
+      };
+   });
 
    socket.on('startStroke', (line) => {
+      if (line.sessionID == sessionID) {
+         return;
+      }
       startStroke(line);
    })
 
    socket.on('endStroke', (line) => {
+      if (line.sessionID == sessionID) {
+         return;
+      }
       endStroke(line);
    })
 
    socket.on('stroke', (line) => {
+      if (line.sessionID == sessionID) {
+         return;
+      }
       stroke(line);
    });
 
@@ -25,14 +52,6 @@ setTimeout(() => {
    const canvasOffsetY = canvas.offsetTop;
    canvas.width = window.innerWidth - 2*canvasOffsetX;
    canvas.height = window.innerHeight - canvasOffsetY;
-
-   let current = {
-      drawing: false,
-      color: 'black',
-      lineWidth: '2',
-      x: 0,
-      y: 0,
-   };
 
    function stroke(line) {
       if (!line.drawing) return;
@@ -49,7 +68,7 @@ setTimeout(() => {
       current.y = line.y;
    }
 
-   function endStroke() {
+   function endStroke(line) {
       current.drawing = false;
       context.stroke();
       context.beginPath();
@@ -59,24 +78,42 @@ setTimeout(() => {
       current.x = e.clientX;
       current.y = e.clientY;
       startStroke(current);
+      // if(!current.emit) {
+      //    console.log("Don't emit");
+      //    return;
+      // }
+      //current.emit = false;
       socket.emit('startStroke', current);
+      //current.emit = true;
    });
 
    canvas.addEventListener('mousemove', function (e) {
       current.x = e.clientX;
       current.y = e.clientY;
       stroke(current);
+      //if(!current.emit) return;
+
+      //current.emit = false;
       socket.emit('stroke', current);
+      //current.emit = true;
    });
 
    canvas.addEventListener('mouseup', function (e) {
-      endStroke();
-      socket.emit('endStroke');
+      endStroke(current);
+      // if(!current.emit) return;
+
+      // current.emit = false;
+      socket.emit('endStroke', current);
+      //current.emit = true;
    });
 
    canvas.addEventListener('mouseleave', () => {
-      endStroke();
-      socket.emit('endStroke');
+      endStroke(current);
+      // if(!current.emit) return;
+
+      // current.emit = false;
+      socket.emit('endStroke', current);
+      //current.emit = true;
    });
 
    function clear() {
@@ -107,112 +144,3 @@ setTimeout(() => {
       }
    })
 }, "500")
-// document.addEventListener('DOMContentLoaded', function () {
-//    let canvas = document.getElementById('whiteboard');
-//    let context = canvas.getContext('2d');
-//    let toolbar = document.getElementById('toolbar');
-
-//    const socket = io.connect("https://websocket-test-ad1c.onrender.com");
-//    console.log("asdf")
-
-//    socket.on('startStroke', (line) => {
-//       startStroke(line);
-//    })
-
-//    socket.on('endStroke', (line) => {
-//       endStroke(line);
-//    })
-
-//    socket.on('stroke', (line) => {
-//       stroke(line);
-//    });
-
-//    socket.on('clear', clear);
-
-//    // Set the canvas size
-//    const canvasOffsetX = canvas.offsetLeft;
-//    const canvasOffsetY = canvas.offsetTop;
-//    canvas.width = window.innerWidth - 2*canvasOffsetX;
-//    canvas.height = window.innerHeight - canvasOffsetY;
-
-//    let current = {
-//       drawing: false,
-//       color: 'black',
-//       lineWidth: '2',
-//       x: 0,
-//       y: 0,
-//    };
-
-//    function stroke(line) {
-//       if (!line.drawing) return;
-//       context.lineWidth = line.lineWidth;
-//       context.lineCap = 'round';
-//       context.strokeStyle = line.color
-//       context.lineTo(line.x-canvasOffsetX, line.y-canvasOffsetY);
-//       context.stroke();
-//    }
-
-//    function startStroke(line) {
-//       current.drawing = true;
-//       current.x = line.x;
-//       current.y = line.y;
-//    }
-
-//    function endStroke() {
-//       current.drawing = false;
-//       context.stroke();
-//       context.beginPath();
-//    }
-
-//    canvas.addEventListener('mousedown', function (e) {
-//       current.x = e.clientX;
-//       current.y = e.clientY;
-//       startStroke(current);
-//       socket.emit('startStroke', current);
-//    });
-
-//    canvas.addEventListener('mousemove', function (e) {
-//       current.x = e.clientX;
-//       current.y = e.clientY;
-//       stroke(current);
-//       socket.emit('stroke', current);
-//    });
-
-//    canvas.addEventListener('mouseup', function (e) {
-//       endStroke();
-//       socket.emit('endStroke');
-//    });
-
-//    canvas.addEventListener('mouseleave', () => {
-//       endStroke();
-//       socket.emit('endStroke');
-//    });
-
-//    function clear() {
-//       context.clearRect(0, 0, canvas.width, canvas.height);
-//       console.log("clear");
-//    }
-
-//    toolbar.addEventListener('click', (e) => {
-//       // Clear tool
-//       if(e.target.id === 'clear') {
-//          clear()
-//          socket.emit('clear');
-//       }
-//       // Eraser tool
-//       if (e.target.id === 'eraser') {
-//          current.color = '#FFFFFF';
-//       }
-//    })
-
-//    toolbar.addEventListener('change', (e) => {
-//       // Color picker
-//       if (e.target.id === 'color-picker') {
-//          current.color = e.target.value;
-//       }
-//       // Width picker
-//       if (e.target.id === 'width-picker') {
-//          current.lineWidth = e.target.value;
-//       }
-//    })
-// });
